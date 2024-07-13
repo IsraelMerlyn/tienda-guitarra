980e40ca7adc1da37456666260674a08f9e50889
IsraelMerlyn <merlyn142a@gmail.com>
2024-07-13 13:37:21 -0600

    tasks #1.3 PROPS

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 export const Guitar = ({guitar}) => {
 
-  const {id,name, image, description, price} = guitar
-  
+  const {id,name, image, description, price} = guitar;
+
+  const handleClick = (id) => {
+    console.log('click', id)
+  }
+
   return (
     <>
       <div className="col-md-6 col-lg-4 my-4 row align-items-center">
@@ -20,7 +24,8 @@ export const Guitar = ({guitar}) => {
            {description}
           </p>
           <p className="fw-black text-primary fs-3">${price} </p>
-          <button type="button" className="btn btn-dark w-100">
+          
+          <button onClick={() => handleClick (id)} type="button" className="btn btn-dark w-100">
             Agregar al Carrito
           </button>
         </div>
